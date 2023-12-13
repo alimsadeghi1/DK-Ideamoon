838ef0a36b4acafc804fc20b71b00cdae4a79fa4
Ali M. Sadeghi <alimsadeghi.dev@gmail.com>
2023-12-13 06:36:36 +0330

    first commit

--- Phase2/src/pages/Home/Challenges/ChallengesVideoHome.jsx
+++ Phase2/src/pages/Home/Challenges/ChallengesVideoHome.jsx
@@ -7,14 +7,14 @@ const ChallengesVideoHome = () => {
             <div className={'row'}>
                 <div className={'col-12'}>
                     <div className={'d-flex justify-content-center mt-5 pt-5'}>
-                        <video controls>
+                        {/* <video controls>
                             <source
                                 src={
                                     'https://dhr.digikala.com/api/v1/stream/Teaser.mp4'
                                 }
                                 type='video/mp4'
                             />
-                        </video>
+                        </video> */}
 
                         {/* <video
                             loop
@@ -30,14 +30,14 @@ const ChallengesVideoHome = () => {
                                 type='video/mp4'
                             />
                         </video> */}
-                        {/* <ReactPlayer
+                        <ReactPlayer
                             className={'react-player'}
                             play='true'
                             url={`https://dhr.digikala.com/api/v1/stream/Teaser.mp4`}
                             controls={true}
                             muted={false}
                             progressInterval={100}
-                        /> */}
+                        />
                     </div>
                 </div>
             </div>
